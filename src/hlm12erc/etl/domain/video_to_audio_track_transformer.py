480a67de28ad1ebf31e4dea91e0ffc422d4dbd16
Hudson Mendes <hudsonmendes@gmail.com>
2023-07-13 13:58:46 +0100

    etl, tests

--- src/hlm12erc/etl/domain/video_to_audio_track_transformer.py
+++ src/hlm12erc/etl/domain/video_to_audio_track_transformer.py
@@ -1,6 +1,5 @@
 import pathlib
 import wave
-from typing import Optional
 
 import pandas as pd
 from moviepy.editor import VideoFileClip
@@ -29,19 +28,38 @@ class VideoToAudioTrackTransformer:
         :param row: The row containing the filepath to the video to extract the audio track from.
         :return: The filepath of the extracted audio track.
         """
-        # open the video file to extract the audio track
-        clip = VideoFileClip(row["x_av"])
 
-        # extract the audiotrack
-        audio = clip.audio
+        # reading videos can run into many errors, so we try to read the video
+        # but get ready for not being able to read it and then we just return
+        # an empty wave file.
 
-        # save the audio track or an empty wave to the destination directory with the specified filename
+        # define the filename of the audio track
+        filename, filepath = self._prepare_filepath_destination(row)
+        extracted = False
+        try:
+            # open the video file to extract the audio track
+            # extract the audiotrack and save the audio track
+            clip = VideoFileClip(row["x_av"])
+            audio = clip.audio
+            if audio:
+                audio.write_audiofile(filepath)
+                extracted = True
+        except Exception as e:
+            print(f"Error while reading video: {e}")
+
+        # regardless of whether we received an error or simply could not
+        # extract the audio track, we produce an empty wave file
+        if not extracted:
+            self._produce_empty_wave(filepath)
+
+        return filename
+
+    def _prepare_filepath_destination(self, row):
         filename = f"d-{row['dialogue']}-seq-{row['seq']}.wav"
         filepath = self.dest / filename
         if not filepath.parent.exists():
             filepath.parent.mkdir(parents=True)
-        audio.write_audiofile(filepath) if audio else self._produce_empty_wave(filepath)
-        return filename
+        return filename, filepath
 
     def _produce_empty_wave(self, filepath: pathlib.Path) -> None:
         """
@@ -49,7 +67,7 @@ class VideoToAudioTrackTransformer:
         :param filepath: The filepath to produce the empty wave file at.
         :return: None.
         """
-        with wave.open(str(filepath), "wb+") as f:
+        with wave.open(str(filepath), "wb") as f:
             f.setnchannels(1)
             f.setsampwidth(2)
             f.setframerate(44100)
---
+++ tests/etl/domain/test_video_to_audio_track_producer.py
@@ -0,0 +1,57 @@
+import pathlib
+import shutil
+import unittest
+import wave
+
+import pandas as pd
+
+from hlm12erc.etl.domain.video_to_audio_track_transformer import (
+    VideoToAudioTrackTransformer,
+)
+
+
+class TestVideoToAudioTrackTransformer(unittest.TestCase):
+    def setUp(self):
+        self.mp4 = pathlib.Path("tests/fixtures/dia1_utt0.mp4")
+        self.wav = pathlib.Path("tests/fixtures/dia1_utt0.wav")
+        self.dest = pathlib.Path("/tmp/hlm12erc/test_output/")
+        self.dest.mkdir(exist_ok=True)
+
+    def tearDown(self):
+        shutil.rmtree(self.dest)
+
+    def test_audio_extraction(self):
+        # create a test row with the filepath to the test video clip
+        row = pd.Series({"x_av": str(self.mp4), "dialogue": 1, "seq": 1})
+
+        # create a transformer instance and call it on the test row
+        transformer = VideoToAudioTrackTransformer(self.dest)
+        filename = transformer(row)
+
+        # check that the audio track was extracted and saved to the destination directory
+        filepath = self.dest / filename
+        self.assertTrue(filepath.exists())
+        with wave.open(str(filepath), "rb") as f1, wave.open(str(self.wav), "rb") as f2:
+            self.assertEqual(f1.getnchannels(), f2.getnchannels())
+            self.assertEqual(f1.getsampwidth(), f2.getsampwidth())
+            self.assertEqual(f1.getframerate(), f2.getframerate())
+            self.assertEqual(f1.getnframes(), f2.getnframes())
+            self.assertEqual(f1.readframes(-1), f2.readframes(-1))
+
+    def test_empty_wave_production(self):
+        # create a test row with an invalid filepath
+        noise = pathlib.Path("tests/fixtures/noise_no_audio.mp4")
+        row = pd.Series({"x_av": str(noise), "dialogue": 1, "seq": 1})
+
+        # create a transformer instance and call it on the test row
+        transformer = VideoToAudioTrackTransformer(self.dest)
+        filename = transformer(row)
+
+        # check that an empty wave file was produced and saved to the destination directory
+        filepath = self.dest / filename
+        self.assertTrue(filepath.exists())
+        with wave.open(str(filepath), "rb") as f:
+            self.assertEqual(f.getnchannels(), 1)
+            self.assertEqual(f.getsampwidth(), 2)
+            self.assertEqual(f.getframerate(), 44100)
+            self.assertEqual(f.getnframes(), 0)
