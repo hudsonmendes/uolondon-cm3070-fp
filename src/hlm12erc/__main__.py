ac2372283f49662ae35494e3537bf8ea2dac5a90
Hudson Mendes <hudsonmendes@gmail.com>
2023-07-16 17:36:15 +0100

    ready to run training

--- src/hlm12erc/__main__.py
+++ src/hlm12erc/__main__.py
@@ -1,14 +1,23 @@
 # Python Built-in Modules
 import logging
+import warnings
 
 # Third-Party Libraries
 import fire
+import transformers
 from tqdm import tqdm
 
 # My Packages and Modules
 from hlm12erc import serving
 
 if __name__ == "__main__":
+    # logging settings
     logging.basicConfig(level=logging.INFO)
+    transformers.logging.set_verbosity(transformers.logging.ERROR)
+    warnings.simplefilter("ignore")
+
+    # pandas/tqdm
     tqdm.pandas()
+
+    # launching CLI
     fire.Fire(serving.CLI)
