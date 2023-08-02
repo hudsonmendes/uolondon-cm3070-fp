2351f7947ca2d26d6e6429c5e0cd3bf5a4f1c92e
Hudson Mendes <hudsonmendes@gmail.com>
2023-08-02 14:56:02 +0100

    mlops: data preparation section explained

--- src/hlm12erc/training/__init__.py
+++ src/hlm12erc/training/__init__.py
@@ -1,6 +1,7 @@
 # Local Folders
 from .erc_config_loader import ERCConfigLoader
+from .erc_data_collator import ERCDataCollator
 from .erc_trainer import ERCTrainer
 from .meld_dataset import MeldDataset
 
-__all__ = ["ERCTrainer", "ERCConfigLoader", "MeldDataset"]
+__all__ = ["ERCTrainer", "ERCConfigLoader", "ERCDataCollator", "MeldDataset"]
