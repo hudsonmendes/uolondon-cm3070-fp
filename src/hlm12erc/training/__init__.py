f7b6e4bc7af164147f5a947734b90c782b80b19a
Hudson Mendes <hudsonmendes@gmail.com>
2023-08-07 21:08:37 +0100

    modelling: evaluation pipeline

--- src/hlm12erc/training/__init__.py
+++ src/hlm12erc/training/__init__.py
@@ -1,8 +1,16 @@
 # Local Folders
 from .erc_config_loader import ERCConfigLoader
 from .erc_data_collator import ERCDataCollator
+from .erc_metric_calculator import ERCMetricCalculator
 from .erc_path import ERCPath
 from .erc_trainer import ERCTrainer
 from .meld_dataset import MeldDataset
 
-__all__ = ["ERCTrainer", "ERCConfigLoader", "ERCDataCollator", "ERCPath", "MeldDataset"]
+__all__ = [
+    "ERCTrainer",
+    "ERCConfigLoader",
+    "ERCDataCollator",
+    "ERCMetricCalculator",
+    "ERCPath",
+    "MeldDataset",
+]
