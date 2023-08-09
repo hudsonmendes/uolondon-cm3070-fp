52d6576d281ee82716dfca6d3377d99420edf879
Hudson Mendes <hudsonmendes@gmail.com>
2023-08-09 14:33:29 +0100

    tests: covered, meld_preprocessor_text

--- tests/training/test_meld_preprocessor_audio.py
+++ tests/training/test_meld_preprocessor_audio.py
@@ -13,7 +13,12 @@ class TestMeldAudioPreprocessor(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_call(self):
+    def test_call_creates_tensor_for_audio_1_waveform(self):
         with wave.open("tests/fixtures/d-1038-seq-17.wav") as wave_file:
             result = self.preprocessor(wave_file)
             self.assertEqual(result.shape, (325458,))
+
+    def test_call_creates_tensor_for_audio_2_waveform(self):
+        with wave.open("tests/fixtures/dia1_utt0.wav") as wave_file:
+            result = self.preprocessor(wave_file)
+            self.assertEqual(result.shape, (218736,))
--- tests/training/test_meld_preprocessor_visual.py
+++ tests/training/test_meld_preprocessor_visual.py
@@ -2,7 +2,6 @@
 import unittest
 
 # Third-Party Libraries
-import torch
 from PIL import Image
 
 # My Packages and Modules
@@ -18,7 +17,7 @@ class TestMeldVisualPreprocessor(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_call(self):
+    def test_call_creates_4d_tensor(self):
         with Image.open("tests/fixtures/d-1038-seq-17.png") as image:
             result = self.preprocessor(image)
-            self.assertEqual(result.shape, (3, 256, 721))
+            self.assertEqual(result.shape, self.config.visual_in_features)
