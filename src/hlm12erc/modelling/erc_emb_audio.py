f882b885c48b0bcf1e862b3eb037a1c9a155ad1b
Hudson Leonardo MENDES <hudsonmendes@users.noreply.github.com>
2023-08-20 16:10:28 +0100

    objective-3: advanced audio embeddings (#7)
    * modelling: introduced wav2vec2 embeddings
    
    * modelling: test-covered wav2vec/mean-max pooled audio embeddings
    
    * Created using Colaboratory
    
    * modelling: experiment configs for wav2vec
    
    * Created using Colaboratory

--- src/hlm12erc/modelling/erc_emb_audio.py
+++ src/hlm12erc/modelling/erc_emb_audio.py
@@ -4,6 +4,7 @@ from typing import Callable, Optional, Type
 
 # Third-Party Libraries
 import torch
+import transformers
 from torch.nn.functional import normalize as l2_norm
 
 # Local Folders
@@ -46,6 +47,8 @@ class ERCAudioEmbeddings(ERCEmbeddings):
         """
         if expression == ERCAudioEmbeddingType.WAVEFORM:
             return ERCRawAudioEmbeddings
+        elif expression == ERCAudioEmbeddingType.WAV2VEC2:
+            return ERCWave2Vec2Embeddings
         elif expression == ERCAudioEmbeddingType.NONE:
             return lambda _: None
         raise ValueError(f"Unknown audio embedding type: {expression}")
@@ -101,3 +104,86 @@ class ERCRawAudioEmbeddings(ERCAudioEmbeddings):
         Returns the dimensionality of the vectors produced by the embedding transformation.
         """
         return self.config.audio_out_features
+
+
+class ERCWave2Vec2Embeddings(ERCAudioEmbeddings):
+    """
+    ERCWave2Vec2Embeddings is a class that implements the
+    Audio Feature Extraction model using the pretrained Wave2Vec2 model,
+    by concatenating the mean and max pooling of the hidden states.
+
+    References:
+        >>> Alexei Baevski, Yuhao Zhou, Abdelrahman Mohamed, and Michael Auli.
+        ... 2020. wav2vec 2.0: A Framework for Self-Supervised Learning of Speech
+        ... Representations. In Advances in Neural Information Processing Systems,
+        ... Curran Associates, Inc., 12449–12460. Retrieved
+        ... from https://proceedings.neurips.cc/paper_files/paper/2020/file/92d1e1eb1cd6f9fba3227870bb6d7f07-Paper.pdf
+
+        >>> Minghua Zhang, Yunfang Wu, Weikang Li, and Wei Li. 2018. Learning
+        ... Universal Sentence Representations with Mean-Max Attention Autoencoder.
+        ... In Proceedings of the 2018 Conference on Empirical Methods in Natural
+        ... Language Processing, 4514–4523.
+
+    Example:
+        >>> from hlm12erc.modelling import ERCConfig, ERCAudioEmbeddingType
+        >>> from hlm12erc.modelling.erc_emb_audio import ERCAudioEmbeddings
+        >>> config = ERCConfig()
+        >>> ERCAudioEmbeddings.resolve_type_from(ERCAudioEmbeddingType.WAV2VEC2)(config)
+    """
+
+    in_features: int
+    hidden_size: int
+    wav2vec2: transformers.Wav2Vec2Model
+    fc: torch.nn.Linear | None
+
+    def __init__(self, config: ERCConfig) -> None:
+        """
+        Constructs the Audio Feature Extraction model based on raw audio.
+
+        :param config: configuration for the model
+        """
+        super(ERCWave2Vec2Embeddings, self).__init__(config=config)
+        self.config = config
+        self.in_features = config.audio_in_features
+        self.hidden_size = config.audio_out_features
+        self.wav2vec2 = transformers.Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base")
+        # determine the hidden size of this layer which may either be set by config
+        # or twice the size of the wav2vec2 hidden size (mean+max pooling)
+        wav2vec2_meanmax_dims = self.wav2vec2.config.hidden_size * 2
+        if self.hidden_size is None or self.hidden_size <= 0:
+            self.hidden_size = wav2vec2_meanmax_dims  # mean+max pooling
+        # only create project _if_ the output size is different from the hidden size
+        self.fc = None
+        if wav2vec2_meanmax_dims != self.hidden_size:
+            self.fc = torch.nn.Linear(wav2vec2_meanmax_dims, self.hidden_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Create a representation based on the last hidden state of the pretrained
+        Wave2Vec2 model concatenating the mean and max pooling of the hidden states.
+
+        :param x: stacked vectors representing audio waveforms
+        :return: matrix of tensors (batch_size, out_features)
+        """
+
+        # concatenate the mean and max pooling of the hidden states
+        # to generate a fixed size vector that can be used as input
+        # to the classifier
+        h = self.wav2vec2(x).last_hidden_state
+        h_mean = torch.mean(h, dim=1)
+        h_max = torch.max(h, dim=1)[0]
+        y = torch.cat((h_mean, h_max), dim=1)
+
+        # only projects if the output size is different from the hidden size
+        if self.fc is not None and self.hidden_size != y.size(dim=1):
+            y = self.fc(y)
+
+        # normalize the output vector to have unit norm
+        return l2_norm(y, p=2, dim=1)
+
+    @property
+    def out_features(self) -> int:
+        """
+        Returns the dimensionality of the vectors produced by the wav2vec2 model.
+        """
+        return self.hidden_size
--- tests/modelling/test_erc_emb_audio.py
+++ tests/modelling/test_erc_emb_audio.py
@@ -19,12 +19,12 @@ class TestERCRawAudioEmbeddings(unittest.TestCase):
     def setUp(self):
         self.config = ERCConfig(audio_in_features=325458, audio_out_features=256, classifier_classes=["a", "b"])
         self.embeddings = ERCAudioEmbeddings.resolve_type_from(ERCAudioEmbeddingType.WAVEFORM)(self.config)
-        self.preprocessor = MeldAudioPreprocessor()
         self.data_collator = ERCDataCollator(config=self.config, label_encoder=None)
+        preprocessor = MeldAudioPreprocessor()
         self.audios = self.data_collator._audio_to_stacked_tensor(
             [
-                self.preprocessor(wave.open("tests/fixtures/d-1038-seq-17.wav")),
-                self.preprocessor(wave.open("tests/fixtures/dia1_utt0.wav")),
+                preprocessor(wave.open("tests/fixtures/d-1038-seq-17.wav")),
+                preprocessor(wave.open("tests/fixtures/dia1_utt0.wav")),
             ]
         )
 
@@ -37,6 +37,42 @@ class TestERCRawAudioEmbeddings(unittest.TestCase):
     def test_forward_shape(self):
         output_tensor = self.embeddings(self.audios)
         self.assertEqual(output_tensor.shape, (len(self.audios), self.embeddings.out_features))
+
+    def test_forward_normalization(self):
+        output_tensor = self.embeddings(self.audios)
+        norms = torch.norm(output_tensor, dim=1)
+        for norm in norms:
+            self.assertAlmostEqual(norm.item(), 1.0, places=5)
+
+
+class TestERCWave2Vec2Embeddings(unittest.TestCase):
+    def setUp(self):
+        self.config = ERCConfig(audio_in_features=16000, audio_out_features=32, classifier_classes=["a", "b"])
+        self.embeddings = ERCAudioEmbeddings.resolve_type_from(ERCAudioEmbeddingType.WAV2VEC2)(self.config)
+        self.data_collator = ERCDataCollator(config=self.config, label_encoder=None)
+        preprocessor = MeldAudioPreprocessor()
+        self.audios = self.data_collator._audio_to_stacked_tensor(
+            [
+                preprocessor(wave.open("tests/fixtures/d-1038-seq-17.wav")),
+                preprocessor(wave.open("tests/fixtures/dia1_utt0.wav")),
+            ]
+        )
+
+    def tearDown(self):
+        del self.embeddings
+
+    def test_out_features(self):
+        self.assertEqual(self.embeddings.out_features, self.config.audio_out_features)
+
+    def test_forward_shape_using_projection(self):
+        output_tensor = self.embeddings(self.audios)
+        self.assertEqual(output_tensor.shape, (len(self.audios), self.embeddings.out_features))
+
+    def test_forward_shape_without_projection(self):
+        config = ERCConfig(audio_in_features=16000, audio_out_features=-1, classifier_classes=["a", "b"])
+        embeddings = ERCAudioEmbeddings.resolve_type_from(ERCAudioEmbeddingType.WAV2VEC2)(config)
+        output_tensor = embeddings(self.audios)
+        self.assertEqual(output_tensor.shape, (len(self.audios), embeddings.wav2vec2.config.hidden_size * 2))
 
     def test_forward_normalization(self):
         output_tensor = self.embeddings(self.audios)
