4975d2bf4878bbf624a223931fa5853f91653e52
Hudson Mendes <hudsonmendes@gmail.com>
2023-09-09 18:12:59 +0100

    training: removed separated random/seed

--- src/hlm12erc/modelling/erc_config.py
+++ src/hlm12erc/modelling/erc_config.py
@@ -71,7 +71,6 @@ class ERCConfig:
     classifier_warmup_steps: int = field(default=500)
     classifier_epsilon: float = field(default=1e-8)
     classifier_loss_fn: str = field(default=ERCLossFunctions.CROSSENTROPY)
-    classifier_seed: int = field(default=42)
     classifier_early_stopping_patience: int | None = field(default=None)
     classifier_metric_for_best_model: str = field(default="loss")
 
